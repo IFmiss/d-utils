9fe50d3343854038dede44224bd33db67b683d1a
ifmiss <185098535@qq.com>
2019-04-02 00:46:27 +0800

    :pencil2: 【Change】代码改为ts

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,10 @@ import './style.scss'
  * @description 关于通用js的收录以及整合方便日后使用
  */
 // dom操作
-import D_JS_UTILS from './lib/index'
+// import D_JS_UTILS from './lib/index'
 import PerformanceUtils from './lib/performanceUtils/index.ts'
-window.Dutils = D_JS_UTILS
+// window.Dutils = D_JS_UTILS
 
-D_JS_UTILS.device.checkLayoutOrientation()
-console.log(PerformanceUtils.loadTime())
-export default D_JS_UTILS
+// D_JS_UTILS.device.checkLayoutOrientation()
+console.log(PerformanceUtils)
+// export default D_JS_UTILS
--- src/lib/performanceUtils/index.ts
+++ src/lib/performanceUtils/index.ts
@@ -1,52 +1,74 @@
 export default class PerformanceUtils {
-  static performance = window.performance
-  static timing = window.performance.timing
   /**
-   * DNS查询耗时
+   * @description window.performance对象
+   */
+  static performance = window.performance
+
+  /**
+   * @description window.performance.timing对象
+   */
+  static timing = window.performance.timing
+
+  /**
+   * @description DNS查询耗时
+   * @description timing.domainLookupEnd - timing.domainLookupStart
+   * @returns { number } 时差 单位：ms
    */
   static dnsTime (): number {
     return PerformanceUtils.timing.domainLookupEnd - PerformanceUtils.timing.domainLookupStart
   }
 
   /**
-   * 白屏时间
+   * @description 白屏时间
+   * @description timing.domLoading - timing.navigationStart
+   * @returns { number } 时差 单位：ms
    */
   static loadTime (): number {
     return PerformanceUtils.timing.domLoading - PerformanceUtils.timing.navigationStart
   }
 
   /**
-   * request请求耗时
+   * @description request请求耗时
+   * @description timing.responseEnd - timing.responseStart
+   * @returns { number } 时差 单位：ms
    */
   static requestTime (): number {
     return PerformanceUtils.timing.responseEnd - PerformanceUtils.timing.responseStart
   }
 
   /**
-   * TCP链接耗时
+   * @description TCP链接耗时
+   * @description timing.connectEnd - timing.connectStart
+   * @returns { number } 时差 单位：ms
    */
   static tcpTime (): number {
     return PerformanceUtils.timing.connectEnd - PerformanceUtils.timing.connectStart
   }
 
   /**
-   * 解析dom树耗时
+   * @description 解析dom树耗时
+   * @description timing.domComplete - timing.domInteractive
+   * @returns { number } 时差 单位：ms
    */
   static renderDomTime (): number {
     return PerformanceUtils.timing.domComplete - PerformanceUtils.timing.domInteractive
   }
 
   /**
-   * domready时间(用户可操作时间节点)
+   * @description domready时间(用户可操作时间节点)
+   * @description timing.domContentLoadedEventEnd - timing.navigationStart
+   * @returns { number } 时差 单位：ms
    */
   static readyDomTime (): number {
     return PerformanceUtils.timing.domContentLoadedEventEnd - PerformanceUtils.timing.navigationStart
   }
 
   /**
-   * onload时间(总下载时间)
+   * @description onload时间(总下载时间)
+   * @description timing.loadEventEnd - timing.navigationStart
+   * @returns { number } 时差 单位：ms
    */
-  static loadAllTime (): number {
+  static loadFullTime (): number {
     return PerformanceUtils.timing.loadEventEnd - PerformanceUtils.timing.navigationStart
   }
 }
