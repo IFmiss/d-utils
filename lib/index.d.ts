80541a8a119bd1c0367a49e353e1a37cc027388a
DavidDai <185098535@qq.com>
2019-06-24 15:12:30 +0800

    修改ts生成的地址路径

---
+++ lib/index.d.ts
@@ -0,0 +1,92 @@
+/**
+ * @author ifmiss
+ * @version 2.0
+ * @description 关于通用js的收录以及整合方便日后使用
+ */
+import * as dom from './domUtils/index';
+import * as utils from './genericUtils/index';
+import * as store from './storeUtils/index';
+import * as exp from './expUtils/index';
+import * as device from './deviceUtils/index';
+import weixin from './weixinUtils/index';
+import performance from './performanceUtils/index';
+import log from './logUtils/index';
+import * as url from './urlUtils/index';
+import http from './httpRequestUtils/index';
+import img from './imageUtils/index';
+import * as decorator from './decoratorUtils/index';
+import event from './eventUtils/index';
+import promise from './promiseUtils/index';
+/**
+ * Dom相关静态类
+ */
+export declare const DomUtils: typeof dom;
+/**
+ * 装饰器
+ */
+export declare const DecoratorUtils: typeof decorator;
+/**
+ * 性能相关静态类
+ */
+export declare const PerformanceUtils: typeof performance;
+/**
+ * 设备相关静态类
+ */
+export declare const DeviceUtils: typeof device;
+/**
+ * 数据存储静态类
+ */
+export declare const StoreUtils: typeof store;
+/**
+ * 日志 打印静态类
+ */
+export declare const LogUtils: typeof log;
+/**
+ * 通用工具静态类
+ */
+export declare const GenericUtils: typeof utils;
+/**
+ * url相关
+ */
+export declare const UrlUtils: typeof url;
+/**
+ * 微信相关
+ */
+export declare const WeixinUtils: typeof weixin;
+/**
+ * 正则相关静态类
+ */
+export declare const ExpUtils: typeof exp;
+/**
+ * axios二次封装
+ */
+export declare const HttpRequestUtils: typeof http;
+/**
+ * 图片合成类
+ */
+export declare const ImageUtils: typeof img;
+/**
+ * 事件相关
+ */
+export declare const EventUtils: typeof event;
+/**
+ * promise相关
+ */
+export declare const PromiseUtils: typeof promise;
+declare const _default: {
+    DecoratorUtils: typeof decorator;
+    DeviceUtils: typeof device;
+    DomUtils: typeof dom;
+    GenericUtils: typeof utils;
+    StoreUtils: typeof store;
+    ExpUtils: typeof exp;
+    LogUtils: typeof log;
+    WeixinUtils: typeof weixin;
+    PerformanceUtils: typeof performance;
+    UrlUtils: typeof url;
+    HttpRequestUtils: typeof http;
+    ImageUtils: typeof img;
+    EventUtils: typeof event;
+    PromiseUtils: typeof promise;
+};
+export default _default;
---
+++ lib/weixinUtils/index.d.ts
@@ -0,0 +1,95 @@
+import { IWxSign, IWxShareToFriend, IWxShareToFriendsCircle, IWxConfig } from './types';
+/**
+ * 微信相关的工具
+ * 微信jssdk的操作
+ */
+export default class WeixinUtils {
+    static wx: any;
+    /**
+     * @description 初始化微信请求 js-sdk 的url地址 需要区分两种情况
+     * IOS 或者 Android 微信版本小于6.3.31, Android 微信版本大于6.3.31
+     * 当前这种只支持与VUE单页面模式
+     * @returns 返回获取jssdk的url参数值
+     */
+    private static defaultShareInfo;
+    static sdkUrlIosOrAndorid(): string;
+    /**
+     * @description IOS 或者 Android 微信版本小于6.3.31 需要种植首次进入页面的URL，用于解决微信签名错误
+     */
+    static plantSdkUrlIosOrAndorid(): void;
+    /**
+     * @description wxSign
+     * @param { String }  jsapi_ticket  公众号用于调用微信JS接口的临时票据
+     */
+    static wxSign(ticket: string): IWxSign;
+    /**
+     * 跳转微信oauth2授权登录
+     * @param { String }  appId
+     */
+    static routerAuthorized(appId: string): void;
+    /**
+     * randomWord 产生任意长度随机字母数字组合
+     * min-任意长度最小位[固定位数]
+     * max-任意长度最大位
+     */
+    private static randomWord;
+    /**
+     * @description 是否高于微信某一个版本
+     * @param { String } version
+     * @returns { Boolean } 返回是否满足条件
+     */
+    private static isUpThanWxVersion;
+    /**
+     * @description 初始化微信配置签名
+     * @param { Object } data  微信的签名配置
+     * @props { Boolean } data.debug  开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+     * @props { String } data.appId  必填，公众号的唯一标识
+     * @props { Number } data.timestamp  必填，生成签名的时间戳
+     * @props { String } data.nonceStr  必填，生成签名的随机串
+     * @props { String } data.signature  必填，签名
+     * @props { Array } data.jsApiList  必填，需要使用的JS接口列表
+     * @link 接口列表地址 https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421141115
+     */
+    static initWxConfig(config: IWxConfig): void;
+    /**
+     * 分享给朋友
+     * @param {Object} sharInfo
+     * @props { String } sharInfo.title 分享的title
+     * @props { String } sharInfo.desc 分享描述
+     * @props { String } sharInfo.link 分享链接
+     * @props { String } sharInfo.imgUrl 分享图标
+     * @props { Function } sharInfo.success 成功的回调
+     * @props { Function } sharInfo.cancel  取消的回调
+     * @props { Function } sharInfo.complete 完成的回调
+     */
+    static wxShareToFriend(sharInfo: IWxShareToFriend): Promise<string>;
+    /**
+     * 分享到朋友圈
+     * @param {Object} sharInfo
+     * @props { String } sharInfo.title 分享的title
+     * @props { String } sharInfo.link 分享链接
+     * @props { String } sharInfo.imgUrl 分享图标
+     * @props { Function } sharInfo.success 成功的回调
+     * @props { Function } sharInfo.cancel  取消的回调
+     * @props { Function } sharInfo.complete 完成的回调
+     */
+    static wxShareToFriendCircle(sharInfo: IWxShareToFriendsCircle): Promise<string>;
+    /**
+     * 隐藏所有非基础按钮接口
+     */
+    static hideAllNonBaseMenuItem(): Promise<string | object>;
+    /**
+     * 批量隐藏功能按钮接口
+     * @param { array } arr // 要隐藏的菜单项，只能隐藏“传播类”和“保护类”按钮，所有menu项见附录3
+     */
+    static hideMenuItems(arr?: string[]): Promise<string | object>;
+    /**
+     * @description 微信分享初始化
+     * @param { Object } sharInfo  分享的内容
+     * @props { String } sharInfo.title 分享的title
+     * @props { String } sharInfo.desc 分享描述
+     * @props { String } sharInfo.link 分享链接
+     * @props { String } sharInfo.imgUrl 分享图标
+     */
+    static wxShare(sharInfo: any): Promise<string>;
+}
