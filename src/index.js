50fef9d5979595d04c1599ec8773254a07ab6ceb
DavidDai <185098535@qq.com>
2019-09-06 18:54:33 +0800

    移除部分代码

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.less'
-/**
- * @author ifmiss
- * @version 1.1.0
- * @description 关于通用js的收录以及整合方便日后使用
- */
-// dom操作
-import D_JS_UTILS from './lib/index.js'
-window.Dutils = D_JS_UTILS
