b87c033bb9f922b156b4765a13ac6f20fb763998
ifmiss <185098535@qq.com>
2020-05-24 19:15:18 +0800

    feat: 添加 use方法

---
+++ es/omitStr.js
@@ -0,0 +1,17 @@
+/**
+ * @description 格式化替换字符串
+ * @param { string } str 需要操作的字符串
+ * @param { number } start  开始位置
+ * @param { number } end  结束位置 3 的话就是倒数第三个 倒数第三个不被替换
+ * @param { string } replaceStr  替换的值 默认 '*'
+ * @example
+ * console.log(omitStr('185098535', 2, 3, '*'))
+ * // 18****535
+ */
+function omitStr(str, start, end, replaceStr = '*') {
+    const startPosition = start || 0;
+    const endPosition = end || 0;
+    const count = str.length - startPosition - endPosition;
+    return str.substr(0, startPosition) + replaceStr.repeat(count) + str.substr(count + startPosition, end);
+}
+export default omitStr;
---
+++ es/useTitle.d.ts
@@ -0,0 +1,1 @@
+export default function (titleStr: string): void;
