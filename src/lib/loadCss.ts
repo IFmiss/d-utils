34d8f9bffb58d7cafeaf484c7dd701b107d77e4c
ifmiss <185098535@qq.com>
2020-07-18 14:39:13 +0800

    update: 新增  方法

---
+++ src/lib/loadCss.ts
@@ -0,0 +1,23 @@
+/**
+ * 动态加载css样式
+ * @param url link的地址
+ * @param config link属性配置
+ * @returns Promise<void>
+ */
+function loadCss (url: string, config?: Partial<Omit<HTMLLinkElement, 'href'>>): Promise<Event> {
+  return new Promise((resolve, reject) => {
+    try {
+      const link = document.createElement('link');
+      link.href = url;
+      for (let k in config) {
+        link[k] = config[k]
+      }
+      document.getElementsByTagName('head')[0].appendChild(link);
+      resolve()
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+export default loadCss
---
+++ src/lib/loadScript.ts
@@ -0,0 +1,25 @@
+/**
+ * @description 动态加载script标签
+ * @param url  script 的地址
+ * @param config  script配置
+ * @returns Promise<Event> onload的 e
+ */
+function loadScript (url: string, config?: Partial<Omit<HTMLScriptElement, 'src' | 'onload' | 'onerror' >>): Promise<Event> {
+  return new Promise((resolve, reject) => {
+    try {
+      const body = document.body || document.getElementsByTagName('body')[0]
+      const script = document.createElement('script')
+      script.src = url
+      script.onload = resolve
+      script.onerror = reject
+      for (let k in config) {
+        script[k] = config[k]
+      }
+      body.appendChild(script)
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+export default loadScript
