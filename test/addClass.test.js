3d1eaed71f54b1f6806f806825ccd8e86ff841d2
ifmiss <185098535@qq.com>
2020-07-30 19:02:47 +0800

    update: jsdom

--- test/addClass.test.js
+++ test/addClass.test.js
@@ -1,9 +1,20 @@
 const assert = require('assert')
+const jsdom = require("jsdom");
+const addClass = require('./../src/lib/addClass')
+const { JSDOM } = jsdom;
+const dom = new JSDOM(``, {
+  url: "https://daiwei.site/",
+  referrer: "https://daiwei.site/",
+  contentType: "text/html",
+  includeNodeLocations: true,
+  storageQuota: 10000000
+});
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1)
-    })
+const body = dom.window.document.body
+
+describe('addClass', function() {
+  it('should return -1 when the value is not present', function() {
+    // assert.equal([1, 2, 3].indexOf(4), -1)
+    addClass(body, 'test-class')
   })
 })
